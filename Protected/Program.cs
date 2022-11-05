262800b73bb5f140c5d6b3f26cd2e53aedd9dc98
ARUN VIJAY <ARUN VIJAY@ARUNVJX1>
2022-11-05 00:30:47 -0900

    Add project files.

---
+++ Protected/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Protected.com.checking;
+
+namespace Protected
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            test t = new com.checking.test();
+            display check = new com.checking.display();
+            check.victory("Arun Vijay");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Protected/com/checking/testing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protected.com.checking
+{
+    class test
+    {
+        protected string name;
+    }
+
+    class display : test
+    {
+        public void victory(string n)
+        {
+            name = n;   
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+        }
+    }
+    internal class testing
+    {
+    }
+}
